8aac0b5be301ce7909230ed4f9c6d75e46f04962
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2023-11-01 23:10:39 -0300

    form pedidos captura en {each}

--- src/routes/(auth)/pedidos/+page.server.js
+++ src/routes/(auth)/pedidos/+page.server.js
@@ -1,53 +1,38 @@
-//import { error, redirect } from '@sveltejs/kit';
 import { BASE_URL } from '../../../lib/utils.js';
-import {fetchApi} from '../../../lib/fetchApi.js';
-
-/* let pedidos=[{
-	date: '07/07/2023',
-	date2:'08/07/2023',
-	pedido:6,
-	razon_social:'La facultad sa.',
-	producto:'Palta77',
-	cantidad: 2,
-	precio:300,
-	subtotal:600,
-    }
-];
-*/
-
-
+import { fetchApi } from '../../../lib/fetchApi.js';
 
 export const load = async ({ locals }) => {
-	const getProductos = async () => {
-		return await fetchApi.get({url: BASE_URL + "/productos", token: locals.token, resStatus: 200})
-	  }
-	 
-	const getClientes = async () => {
-	  return await fetchApi.get({url: BASE_URL + "/clientes", token: locals.token, resStatus: 200})
-	}
-	  return {
-		productos: getProductos(),
-		clientes: getClientes(),
-		//pedidos
-	  } 
-  }  
+  
+  const getProductos = async () => {
+    return await fetchApi.get({ url: BASE_URL + "/productos", token: locals.token, resStatus: 200 });
+  };
 
- 
-  export const actions= {
-	default: async ({ request}) => {
-	  const formData = await request.formData();
-	  const date = formData.get('date');
-	  const date2 = formData.get('date2');
-	  const pedido_id = formData.get('pedido_id');
-	  const razon_social = formData.get('selectedRazonSocial');
-	  const producto = formData.get('selectedProducto');
-	  const cantidad = formData.get('cantidad');
-	  const precio = formData.get('precio');
-	  const subtotal = formData.get('subtotal');
-	  
-     console.log( date, date2, pedido_id, razon_social, producto, cantidad, precio, subtotal)
+  const getClientes = async () => {
+    return await fetchApi.get({ url: BASE_URL + "/clientes", token: locals.token, resStatus: 200 });
+  };
 
-	}
-}
+  return {
+    productos: getProductos(),
+    clientes: getClientes(),
+  };
+};
 
+export const actions = {
+  default: async ({ request }) => {
+    const formData = await request.formData();
+    const date = formData.get('date');
+    const date2 = formData.get('date2');
+    const pedido_id = formData.get('pedido_id');
+    const razon_social = formData.get('selectedRazonSocial');
+    const producto = formData.get('selectedProducto');
+    const cantidad = formData.get('cantidad');
+    const precio = formData.get('precio');
+    const subtotal = formData.get('subtotal');
 
+    const pedido = { date, date2, pedido_id, razon_social, producto, cantidad, precio, subtotal };
+
+    
+
+    return pedido;
+  }
+};
--- src/routes/(auth)/pedidos/+page.svelte
+++ src/routes/(auth)/pedidos/+page.svelte
@@ -1,115 +1,125 @@
 <script>
-	
-export let data;
-export let form;
-export const { productos, clientes}=data;
+	export let data;
+	export let form;
+	export const { productos, clientes } = data;
+  
+	let selectedRazonSocial;
+	let selectedProducto;
+	let selectedCantidad;
+	let precio;
+	let formData = [];
+  
+	function handleProductoChange(event) {
+	  const selectedIndex = event.target.selectedIndex;
+	  const selectedOption = event.target.options[selectedIndex];
+	  precio = selectedOption.getAttribute('data-precio');
+	}
+  
+	function handleSubmit(event) {
+	  event.preventDefault();
+	  const newFormData = {
+		date: event.target.date.value,
+		date2: event.target.date2.value,
+		pedido_id: event.target.pedido_id.value,
+		selectedRazonSocial,
+		selectedProducto,
+		selectedCantidad,
+		precio,
+		subtotal: selectedCantidad * precio,
+	  };
+	  formData = [...formData, newFormData];
+	  console.log(formData);
+ 
 
-
-let selectedRazonSocial;
-let selectedProducto;
-let selectedCantidad;
-let precio;
-
-  function handleProductoChange(event) {
-    const selectedIndex = event.target.selectedIndex;
-    const selectedOption = event.target.options[selectedIndex];
-    precio = selectedOption.getAttribute('data-precio');
-  }
-
-</script>
-
-<svelte:head>
+	}
+  </script>
+  
+  <svelte:head>
 	<title>Pedidos</title>
 	<meta name="description" content="Pedidos" />
-</svelte:head>
-
-<h2>Gestion de Pedidos</h2>
-
-<main>
- <article>
-<article class="container">
-	<form method="POST" >
-		<input type="date" name="date">
+  </svelte:head>
+  
+  <h2>Gestion de Pedidos</h2>
+  
+  <main>
+	<article class="container">
+	  <form method="POST" on:submit={handleSubmit}>
+		<input type="text" name="date" value={new Intl.DateTimeFormat('es', { day: 'numeric', month: 'numeric', year: 'numeric' }).format(new Date())}>
 		<input type="date" name="date2">
-		<input type="text" name="pedido_id" value={Math.floor((Math.random()*(10000-1))+1)}/>
+		<input type="text" name="pedido_id" value={Math.floor((Math.random() * (10000 - 1)) + 1)} />
 		<select name="selectedRazonSocial" bind:value={selectedRazonSocial} required>
-			<option selected >Clientes</option>
-			{#each data.clientes as cli} 
-			  <option value={cli.razon_social}>{cli.razon_social}</option>
-			{/each}
+		  <option selected>Clientes</option>
+		  {#each data.clientes as cli}
+			<option value={cli.razon_social}>{cli.razon_social}</option>
+		  {/each}
 		</select>
 		<select name="selectedProducto" bind:value={selectedProducto} on:change={handleProductoChange} required>
-			<option selected>Productos</option>
-			{#each data.productos as prod}
-			  <option value={prod.nombre} data-precio={prod.precio}>{prod.nombre}</option>
-			{/each}
-		  </select>
-		<input type="text" name="cantidad" bind:value={selectedCantidad} required />
-		<input type="text" name="precio" bind:value={precio} required />
-		<input type="number" name="subtotal" value={selectedCantidad*precio} />
+		  <option selected>Productos</option>
+		  {#each data.productos as prod}
+			<option value={prod.nombre} data-precio={prod.precio}>{prod.nombre}</option>
+		  {/each}
+		</select>
+		<input type="text" name="selectedCantidad" bind:value={selectedCantidad} required />
+		<input type="text" name="precio" value={precio} readonly />
+		<input type="text" name="subtotal" value={selectedCantidad * precio} readonly />
 		<button type="submit">add</button>
-	</form>
-</article>
-
-   <table role="grid" class="container">
-    <thead>
-     <tr>
-	<th scope="col">F_Ing.</th>
-	<th scope="col">F_Entrega.</th>
-	<th scope="col">#Pedido_id</th>
-	<th scope="col">Cliente</th>
-	<th scope="col">Producto</th>
-	<th scope="col">Cantidad</th>
-	<th scope="col">Precio</th>
-	<th scope="col">SubTotal</th>
-	<th scope="col">Accion</th>
-	<th scope="col">Accion2</th>
-	<th scope="col">Entregado</th>
-  </tr>
-</thead>
-
-<!---- {#each data as dat}---->
- <tbody>
-	<tr>
-    <td>1</td>
-	<td>7/7/2023</td>
-	<td>#54776</td>
-	<td>cliente</td>
-	<td>Lechuga</td>
-	<td>2</td>
-	<td>$ 300</td>
-	<td>$ 600</td>
-    <td><button type="submit">Editar</button></td>
-	<td><button type="submit">Eliminar</button></td>
-	<td><label for="switch">
-		<input type="checkbox" id="switch" name="switch" role="switch">
-		</label>
-	</td>
-	</tr>
-	</tbody>
-
-   <!----{/each}-->
-   <tfoot>
-    <tr>
-      <th scope="col"></th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td>Total</td>
-	  <td>Total</td>
-	  <td>Total</td>
-	  <td>Total</td>
-	  <td></td>
-    </tr>
-  </tfoot> 	
-   </table>
-
-</article>
-
-</main>
-
-<style>
-
+	  </form>
+  
+	  <table role="grid" class="container">
+		<thead>
+		  <tr>
+			<th scope="col">Fecha</th>
+			<th scope="col">Fecha2</th>
+			<th scope="col">Pedido ID</th>
+			<th scope="col">Cliente</th>
+			<th scope="col">Producto</th>
+			<th scope="col">Cantidad</th>
+			<th scope="col">Precio</th>
+			<th scope="col">SubTotal</th>
+			<th scope="col">Acción</th>
+			<th scope="col">Acción2</th>
+			<th scope="col">Entregado</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  {#each formData as formD }
+			<tr>
+			  <td>{formD.date}</td>
+			  <td>{formD.date2}</td>
+			  <td>{formD.pedido_id}</td>
+			  <td>{formD.selectedRazonSocial}</td>
+			  <td>{formD.selectedProducto}</td>
+			  <td >{formD.selectedCantidad}</td>
+			  <td >{formD.precio}</td>
+			  <td >{formD.subtotal}</td>
+			  <td><button type="submit">Editar</button></td>
+			  <td><button type="submit">Eliminar</button></td>
+			  <td>
+				<label for="switch">
+				  <input type="checkbox" id="switch" name="switch" role="switch">
+				</label>
+			  </td>
+			</tr>
+		  {/each}
+		</tbody>
+		<tfoot>
+		  <tr>
+			<th scope="col"></th>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>Total</td>
+			<td>Total</td>
+			<td>Total</td>
+			<td></td>
+			<td></td>
+		  </tr>
+		</tfoot>
+	  </table>
+	</article>
+  </main>
+  
+  <style>
 	
-</style>
+  </style>
