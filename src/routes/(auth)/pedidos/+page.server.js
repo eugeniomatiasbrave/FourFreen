d2d90f251b7e5be92ee1c3eb0535701a07d81017
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2023-11-10 10:43:27 -0300

    ..

--- src/routes/(auth)/pedidos/+page.server.js
+++ src/routes/(auth)/pedidos/+page.server.js
@@ -51,7 +51,7 @@ export const actions = {
       });
   
     if (res.status === 200) {
-     
+      
       return { success: true };
     } else {
       return { success: false };
@@ -63,87 +63,85 @@ export const actions = {
  }
 };
 
-/*
-delete: async ({ request, locals }) => {
-  const formData = await request.formData();
-  const id = formData.get('pedido_id');
-  console.log("Pedido:",id,locals.token,BASE_URL)
-},
-  /*
-  try {
-    const res=await fetchApi.delete({
-      url: BASE_URL + `/pedidos/${id}`,
-      token: locals.token,
-      body:{
-        "id": id
-      },
-      resStatus: 200
-      });
-    if (res.status === 200) {
-      const datos = await res.json();
-       cookies.set('PedidoEliminado', JSON.stringify({
-       id: datos.id
-       }), {
-        httpOnly: true,
-        path: '/',
-        secure: true,
-        sameSite: 'strict',
-        maxAge: 60 * 60 * 24 // se requiere??
-        })      
-      return { success: true };
-    } else {
-      return { success: false };
-    }
-  } catch (err) {
-    console.log('Error: ', err);
-    throw error(500, 'Algo salió mal al eliminar el pedido');
-  }
-},
-*/
 
-/*
-editar: async ({ request, locals }) => {
-  const formData = await request.formData();
-  const id = formData.get('pedido_id');
-  const date2 = formData.get('date2');
-  const cantidad = formData.get('cantidad');
 
-  // otros campos a editar ...
-  
-  console.log("Pedido:",id,date2,cantidad,locals.token,BASE_URL)
-},
-  /*
-  try {
-    const res = await fetchApi.patch({
-      url: BASE_URL + `/pedidos/${id}`,
-      token: locals.token,
-      body: {
-        "date2": date2,
-        "cantidad": parseFloat(cantidad)
-      },
-      resStatus: 200
-    });
-  
-    if (res.status === 200) {
-      const datos = await res.json();
-      cookies.set('PedidoActualizado', JSON.stringify({
-      date2: datos.date2,
-      cantidad: datos.cantidad
-      }), {
-      httpOnly: true,
-      path: '/',
-      secure: true,
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 24 // se requiere???
-      })  
-      return { success: true };
-      } else {
-      return { success: false };
-    }
-    } catch (err) {
-    console.log('Error: ', err);
-    throw error(500, 'Algo salió mal al actualizar el pedido'); 
-  }}
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
