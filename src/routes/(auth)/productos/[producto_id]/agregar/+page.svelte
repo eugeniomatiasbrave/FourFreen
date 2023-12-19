1055c065bafbca6f4962fb43d233b72dc329d1ae
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2023-12-19 13:05:26 -0300

    sweet alert en lugar del Alert()

--- src/routes/(auth)/productos/[producto_id]/agregar/+page.svelte
+++ src/routes/(auth)/productos/[producto_id]/agregar/+page.svelte
@@ -1,25 +1,14 @@
 <script>
-	import {Input,Button,Label, P} from 'flowbite-svelte'
+	import {Input,Button,Label} from 'flowbite-svelte'
+  
 	import {onMount} from 'svelte';
 	import {goto} from '$app/navigation';
 	export let form;
   export let data;
 	export const {productos}=data;
 
-
-//let Productos=productos.datos;
-
-
 let showForm = true;
 
-/*
-const productoExiste = () => {
- let productoExiste= Productos.some(producto => producto.nombre === nombreProducto);
-  if (productoExiste) {
-   alert('Este producto ya existe.'); 
- }
-};
-	*/
 	onMount(() => {
   if (form?.success) {
 	  showForm = false;
@@ -39,30 +28,36 @@ const productoExiste = () => {
   }
 });
 
-
 	</script>
 
 {#if showForm}
  <main class="flex items-center justify-center min-h-screen pb-36 ">
    <div class="bg-white mx-auto p-4 pt-1 my-0 rounded border border-gray-200 shadow-md w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/6"> <!----------------------------modal add productos-->  
       <form class="flex flex-col space-y-4" method="POST" >	
-          <h3 class="mt-4 text-xl text-center font-medium text-gray-900 dark:text-white">Ingrese un producto nuevo </h3>
+          <h3 class="mt-4 text-xl text-center font-medium text-gray-900 dark:text-white">Agregar nuevo producto </h3>
           <Label class="space">
             <span>Producto</span>
             <Input type="text" name="nombre"  maxlength="30" on:change={(e)=> {
                 let nombre = e.target.value;
                 let existe = productos.datos.some(producto => producto.nombre === nombre);
-               if (!existe) {
-                console.log ( `El producto ${nombre} no existe.`)
-                } else{
-                alert(`El producto ${nombre} existe.`);
-                e.target.value = '';
-              }} }        
-              placeholder="Agregar producto" class="bg-white h-7 w-full rounded" required/>
+                if (existe) {
+                  window.Swal.fire( 
+                    {
+                      icon: 'error',
+                      title: 'Producto existente',
+                      text: `Nombre: ${nombre}.`,
+                      backdrop: true,
+                      confirmButtonText: 'Volver',
+                      confirmButtonColor: 'rgb(69, 166, 175)'
+                      });
+                       e.target.value = '';
+                       }
+                      }}      
+                    placeholder="Agregar producto" class="bg-white h-7 w-full rounded" required/>
           </Label>
           <Label class="space">
             <span>Precio</span>
-            <Input type="number" name="precio" min="0" max="1000000" step="0.01" placeholder="Agregar precio" class="bg-white h-7 w-full rounded" required/>
+            <Input type="number" name="precio"  min="0" max="1000000" step="0.01" placeholder="Agregar precio" class="bg-white h-7 w-full rounded" required/>
 			
           </Label> 
           <footer class="text-center">
