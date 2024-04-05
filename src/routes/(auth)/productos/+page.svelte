a9e48ceac3d54c8135c67b27bbc86e6d14b5d4da
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2024-04-05 14:08:29 -0300

    filtrar por columnas /productos  👀

--- src/routes/(auth)/productos/+page.svelte
+++ src/routes/(auth)/productos/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import {goto} from '$app/navigation';
-	import {Input,Button,Table,TableBody,TableBodyCell,TableBodyRow,TableHead,TableHeadCell} from 'flowbite-svelte';			
+	import {Input,Button,Table,TableBody,TableBodyCell,TableBodyRow,TableHead,TableHeadCell} from 'flowbite-svelte';
+	import { Dropdown, Checkbox } from 'flowbite-svelte';			
 	export let data;
 	export const {productos,productosSearch,sortPrecio,searchSortPrecio,sortNombre,sortProducto_id}=data;
 	import Titulo from '$lib/componentes/titulo.svelte';
@@ -82,6 +83,19 @@ const sortedNombre=(params)=> {
     sortedNombre(sortOrder === 1 ? -1 : 1);
   };
 
+  let visibleColumns = {
+	  id: true,
+	  nombre: true,
+	  precio: true,
+	  editar: true,
+	  eliminar: true
+	};
+  
+	// Función para alternar la visibilidad
+	function toggleColumn(column) {
+	  visibleColumns[column] = !visibleColumns[column];
+	}
+
 </script>
 			
 <svelte:head>
@@ -93,6 +107,7 @@ const sortedNombre=(params)=> {
 
 	<!--------------------------Seccion tabla --> 	
 	  <div class="bg-white mx-auto p-1 pt-1 rounded-lg border border-gray-300 shadow-md w-full xl:w-1/2"><!----------------Div contenedor: tabla + add + Filtro--------->
+		
 		<div class="flex flex-col sm:flex-row justify-between items-center mx-auto "> 
 		 <div class="mb-2 sm:mb-0 w-full sm:w-auto"> 
 		  <Button href={`/productos/-1`} data-sveltekit-preload-code="hover" size="xs" class="bg-primary-500 rounded-lg m-0 h-7 px-3 w-full sm:w-auto ">
@@ -109,25 +124,51 @@ const sortedNombre=(params)=> {
 		</div> 
 	  </div>
 			 <div class="overflow-x-auto"> <!-------------------------------Nueva tabla Flowbite-------> 
-				<Table hoverable={true} class="mx-auto mt-1 border text-xs" > 		
+				<Table hoverable={true} class="mx-auto mt-1 border text-xs" >
+					
 				  <TableHead class=" bg-primary-500 text-white" theadClass='text-xs' > <!------------------------cabecera celdas-->
-					<TableHeadCell class="py-2" >
-					  <div class="flex items-center justify-end">
-						Id<button on:click={alternarSortId} class="bg-primary-500 hover:bg-primary-500 rounded">	
-							{#if sortOrder === 1}
-							<svg class="w-2.5 h-4 ms-1.5 text-white-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 8">
-							<path stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="1" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"/>
-							</svg>
-							{:else}
-							<svg class="w-2.5 h-4 ms-1.5 text-white-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 8">
-							<path stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="1" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
-							</svg>		
-						    {/if}
-						 </button>			      
-				      </div>
-				    </TableHeadCell>
-					<TableHeadCell class="py-2">
-					<div class="flex items-center">	
+					<TableHeadCell class="p-0 m-0 w-0 border-r " >
+						<Button class="bg-primary-500 w-0 h-0">👀</Button>
+						<Dropdown class="w-48 p-1 space-y-1 text-sm ">
+							<li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+								<Checkbox  bind:checked={visibleColumns.id} on:click={() => toggleColumn('id')}>Checked Id</Checkbox>
+							</li>
+							<li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+								<Checkbox bind:checked={visibleColumns.nombre} on:click={() => toggleColumn('nombre')}>Checked Nombre</Checkbox>
+							</li>
+							<li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+								<Checkbox bind:checked={visibleColumns.precio} on:click={() => toggleColumn('precio')}>Checked Precio</Checkbox>
+							</li>
+							<li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+								<Checkbox bind:checked={visibleColumns.editar} on:click={() => toggleColumn('editar')}>Checked Editar</Checkbox>
+							</li>
+							<li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+								<Checkbox bind:checked={visibleColumns.eliminar} on:click={() => toggleColumn('eliminar')}>Checked Eliminar</Checkbox>
+							</li>
+						</Dropdown>
+				</TableHeadCell>
+
+					{#if visibleColumns.id} 
+					 <TableHeadCell class="py-2" align="left">
+						<div class="flex items-center ">
+							Id<button on:click={alternarSortId} class="bg-primary-500 hover:bg-primary-500 rounded">	
+								{#if sortOrder === 1}
+								<svg class="w-2.5 h-4 ms-1.5 text-white-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 8">
+									<path stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="1" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"/>
+								</svg>
+								{:else}
+								<svg class="w-2.5 h-4 ms-1.5 text-white-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 8">
+									<path stroke="currentColor" stroke-linecap="butt" stroke-linejoin="round" stroke-width="1" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
+								</svg>		
+								{/if}
+							</button>			      
+						</div>
+				     </TableHeadCell>
+					{/if}
+
+					{#if visibleColumns.nombre}
+					 <TableHeadCell class="py-2">
+					  <div class="flex items-center">	
 					    Nombre
 						<button on:click={alternarSort} class="bg-primary-500 hover:bg-primary-500 rounded">	
 							{#if sortOrder === 1}
@@ -141,8 +182,11 @@ const sortedNombre=(params)=> {
 						    </svg>	
 						    {/if}
 						 </button>
-					  </div>
-				    </TableHeadCell> 
+					   </div>
+				     </TableHeadCell> 
+				    {/if}
+
+					{#if visibleColumns.precio}
 				    <TableHeadCell class="py-2" >
 						<div class="flex items-center justify-end ">	
 						Precio
@@ -160,32 +204,66 @@ const sortedNombre=(params)=> {
 						 </button> 
 					</div>
 				</TableHeadCell>
+				{/if}
+
+				{#if visibleColumns.editar}
 				  <TableHeadCell class="py-2">Editar
 					<span class="sr-only">Editar</span>
 				  </TableHeadCell>
+				{/if}
+
+				{#if visibleColumns.eliminar}
 				  <TableHeadCell class="py-2">Eliminar
-					<span class="sr-only">Eliminar</span>
+					<span class="sr-only"
+					>Eliminar</span>
 				  </TableHeadCell>
+				{/if}
+
 				</TableHead><!----------------------fin cabecera celdas-->	
 				<TableBody class="divide-y ">
-					{#each filteredSortedId as {producto_id, precio, nombre }}
-					<TableBodyRow class=" hover:bg-hover-gray-light ">
-					<TableBodyCell class="py-2" align="right">{producto_id}</TableBodyCell>
-					<TableBodyCell class="py-2">{nombre}</TableBodyCell>
-					<TableBodyCell class="py-2" style="text-align: right;" >${precio.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</TableBodyCell>
-					<TableBodyCell class="py-2">
-						<a  href={`/productos/${producto_id}?action=editar`} data-sveltekit-preload-code="hover"
+					{#each filteredSortedId as prod}
+					<TableBodyRow class=" hover:bg-hover-gray-light">
+						<TableBodyCell class=" w-0 p-0 m-0" align="right"></TableBodyCell>
+
+						{#if visibleColumns.id}
+					     <TableBodyCell class="py-2" align="left">{prod.producto_id}</TableBodyCell>
+						{/if}
+
+						{#if visibleColumns.nombre}
+					     <TableBodyCell class="py-2">{prod.nombre}</TableBodyCell>
+						{/if}
+						
+						{#if visibleColumns.precio}
+					     <TableBodyCell class="py-2" style="text-align: right;" >${prod.precio.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</TableBodyCell>
+						{/if}
+					
+						{#if visibleColumns.editar}
+						 <TableBodyCell class="py-2">
+						  <a  href={`/productos/${prod.producto_id}?action=editar`} data-sveltekit-preload-code="hover"
 							 class="font-medium text-primary-600 hover:underline dark:text-primary-500"  	
 							>Editar</a>			
-					</TableBodyCell>
-					<TableBodyCell class="py-2">		
-						<a href={`/productos/${producto_id}?action=eliminar`} data-sveltekit-preload-code="hover"
-						class="font-medium text-primary-600 hover:underline dark:text-primary-500">Eliminar</a> 
-				    </TableBodyCell>
+					     </TableBodyCell>
+					    {/if}
+
+						{#if visibleColumns.eliminar}
+					     <TableBodyCell class="py-2">		
+					  	  <a href={`/productos/${prod.producto_id}?action=eliminar`} data-sveltekit-preload-code="hover"
+						   class="font-medium text-primary-600 hover:underline dark:text-primary-500">Eliminar</a> 
+				         </TableBodyCell>
+					    {/if}
 				</TableBodyRow>
 				{/each}	
 				</TableBody>
 			  </Table>
 			</div>
 		  </div> <!----------------fin Div contenedor: tabla + add + Filtro--------->	
-	</main>
+		</main>
+
+		<style>	
+		</style>
+	
+
+
+
+	 
+	  
